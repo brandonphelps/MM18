2a6050ebdacfdd7c8fd7f55a377d86677b421b03
Westin <westin@localhost.localdomain>
2016-11-12 14:19:30 -0600

    Added constants file

---
+++ games/saloon/Constants.java
@@ -0,0 +1,10 @@
+package games.saloon;
+
+public class Constants
+{
+	static String NORTH = "North";
+	static String SOUTH = "South";
+	static String EAST = "East";
+	static String WEST = "West";
+
+}
