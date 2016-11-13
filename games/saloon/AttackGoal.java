a22363f2b2ed9f107619917552605f913158314f
Westin <westin@localhost.localdomain>
2016-11-13 08:42:45 -0600

    Fixed some tabs/spaces. Added attacking for all units

--- games/saloon/AttackGoal.java
+++ games/saloon/AttackGoal.java
@@ -26,35 +26,65 @@ public class AttackGoal extends Goal
     {
       if(_game.cowboys.get(i).id.equals(_targetCowboyId))
       {
-	targetCowboy = _game.cowboys.get(i);
-	if(targetCowboy.isDead)
-	{
-	  targetCowboy = null;
-	}
+        targetCowboy = _game.cowboys.get(i);
+        if(targetCowboy.isDead)
+        {
+          targetCowboy = null;
+        }
       }
     }
 
     if(targetCowboy != null)
     {
-      if(cowboy.job.equals("Brawler"))
+      if(cowboy.canMove && !cowboy.isDead)
       {
-	if(cowboy.canMove && !cowboy.isDead)
-	{
-	  List<Tile> path = PathFinder.findPath(cowboy.tile, targetCowboy.tile);
+        List<Tile> path = PathFinder.findPath(cowboy.tile, targetCowboy.tile);
 
-	  if(path.size() > 1)
-	  {
-	    Tile moveTile = path.get(0);
+        if(path.size() > 1)
+        {
+          //Move in place to attack.
+          Tile moveTile = path.get(0);
 
-	    int moveTileDanger = DangerAvoidance.CalculateTileDanger(_game, moveTile, cowboy);
+          int moveTileDanger = DangerAvoidance.CalculateTileDanger(_game, moveTile, cowboy);
 
-	    if(moveTileDanger >= Constants.MEDIUM_DANGER_THRESHOLD)
-	    {
-	      DangerAvoidance.avoidDangerAndMove(_game, cowboy, moveTile);
-	    }
-	    cowboy.move(moveTile);
-	  }
-	}
+          if(path.size() > 3 && moveTileDanger >= Constants.MEDIUM_DANGER_THRESHOLD)
+          {
+            //If we aren't really close, take some move for a medium danger.
+            DangerAvoidance.avoidDangerAndMove(_game, cowboy, moveTile);
+          }
+          else if(path.size() <= 3 && moveTileDanger >= Constants.HIGHISH_DANGER_THRESHOLD)
+          {
+            //If we aren't really close, take some move for a high danger.
+            DangerAvoidance.avoidDangerAndMove(_game, cowboy, moveTile);
+          } else
+          {
+            //There is no significant danger. Just move.
+            cowboy.move(moveTile);
+          }
+        } else
+        {
+          //Time to attack
+
+          if(cowboy.job.equals("Brawler"))
+          {
+            //Do nothing. Brawlers automatically attack.
+          } else if(cowboy.job.equals(Constants.BARTENDER))
+          {
+            if(cowboy.turnsBusy == 0)
+            {
+              cowboy.act(targetCowboy.tile, Constants.NORTH);//TODO: MAYBE BE SMART ABOUT DIRECTION.
+            }
+          } else if(cowboy.job.equals(Constants.SHARPSHOOTER))
+          {
+              cowboy.act(targetCowboy.tile);
+          } else
+          {
+            System.out.println("Invalid job: " + cowboy.job);
+          }
+        }
+      }
+      {
+
       }
     }
     else
--- games/saloon/Constants.java
+++ games/saloon/Constants.java
@@ -12,5 +12,6 @@ public class Constants
 	static final String BARTENDER = "Bartender";
 
 	static final int MEDIUM_DANGER_THRESHOLD = 40;  // Defines minimum danger before we consider using a different path.
+	static final int HIGHISH_DANGER_THRESHOLD = 70;  // Defines minimum danger before we consider using a different path while trying to attack.
 
 }
